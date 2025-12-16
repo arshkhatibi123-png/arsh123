ff9ee151b49faf43b27457f8764f313194344bc7
arshkhatibi123-png <arshkhatibi123@gmail.com>
2025-12-16 13:14:05 +0000

    data types

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,34 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3 + 3)// we are using nodejs , not  browser
+
+console.log(3
+    +
+3) // code redability should be high
+
+console.log("Alisha");
+
+
+let name="Alisha"
+let age = "18"
+let isLoggedIn = false
+
+//primitive
+
+// number=>123
+// number =>
+//bigint=>lrg no
+//string=>"xyz"
+//boolean=>true/false
+//null=> standaloe value
+//undefined => not assigned valued 
+// symbol => uniqness 
+
+
+// non premitive
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
