8c3d1c6c687bcb3b02086f10b3bf36fc071a550e
arshkhatibi123-png <arshkhatibi123@gmail.com>
2025-12-17 12:57:02 +0000

    dataType summary

---
+++ 01_basic/05_summary dataType.js
@@ -0,0 +1,14 @@
+// dataType
+
+// Primitive:
+
+// 7 types: String , no.,boolean(t/f), Null(empty not 0), Undefined , Symbol, BigInt(scientific value or large value; not use commonly) 
+
+const score = 100
+
+// Non primitive(or reference):
+
+// Array, Object ,Functions 
+
+//In javascript we donot define variable ;eg 100 not need to define it is number , false donot need to define it is boolen
+// javascript
