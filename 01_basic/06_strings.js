7951a6a91bf3ca3437a0c89dfe17bdf673dbf3b3
arshkhatibi123-png <arshkhatibi123@gmail.com>
2025-12-18 15:42:26 +0000

    strings

--- 01_basic/06_strings.js
+++ 01_basic/06_strings.js
@@ -7,4 +7,21 @@
 
 console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
 
+const gameName = new String('alishakh')
+
+console.log(gameName[0]);//key access of alishakh
+console.log(gameName.__proto__);// access prototype
+
+console.log(gameName.length);// acess length of alishakh
+
+console.log(gameName.toUpperCase());// changes to upper case // not change original string
+console.log(gameName.indexOf('a'));//finding pstn of d character
+console.log(gameName.charAt(2));//
+
+const newString = gameName.substring(0,5)
+console.log(newString);
+
+
+const anotherString = gameName.slice(-7, -4)
+console.log(anotherString);
 
