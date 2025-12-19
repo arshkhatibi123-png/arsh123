4eaf9cacb45c3ff1eef639e307c82a81d90e339f
arshkhatibi123-png <arshkhatibi123@gmail.com>
2025-12-19 15:51:36 +0000

    nums

--- 01_basic/04_comparison.js
+++ 01_basic/04_comparison.js
@@ -10,13 +10,13 @@ console.log("02">1);//different data type dnt use
 
 
 
-console.log(null>0);//dont use ;avoid
-console.log(null==0);// equalty and comparison work different ; not used this operation.
+console.log(null>0); //dont use ;avoid
+console.log(null==0); // equalty and comparison work different ; not used this operation.
 console.log(null>=0); // comparison convert null to a no ,treat as '0'
 
-console.log(undefined==0);//dont use avoid
-console.log(undefined > 0);//dont use avoid
-console.log(undefined < 0);//dont use avoid 
+console.log(undefined==0); //dont use avoid
+console.log(undefined > 0); //dont use avoid
+console.log(undefined < 0); //dont use avoid 
 
 // ===
 console.log("2"===2);
--- 01_basic/06_strings.js
+++ 01_basic/06_strings.js
@@ -7,7 +7,7 @@
 
 console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('alishakh')
+const gameName = new String('alisha-kh')
 
 console.log(gameName[0]);//key access of alishakh
 console.log(gameName.__proto__);// access prototype
@@ -24,4 +24,36 @@ console.log(newString);
 
 const anotherString = gameName.slice(-7, -4)
 console.log(anotherString);
+
+const newStringOne = "    Arshi      "
+console.log(newStringOne);
+console.log(newStringOne.trim()); // trim remove the unnessary space
+
+const url = "http://arsh123.com/arsh%20khatibi"
+
+console.log(url.replace('%20','_'));// replace string
+console.log(url.includes('arsh'));// true present abv string
+console.log(url.includes('123'));// true present 
+
+console.log(url.includes('alisha'));// false not present
+
+/* Array can hold any of the data types. But, the String can hold only a char data type */
+
+const str = 'My country name is India';
+const words = str.split(' ');// split word 
+console.log(words[2]);
+
+const chars = str.split(' ');
+console.log(chars[5]);
+
+//The JavaScript split() method divides a string into an ordered list of substrings 
+// and returns them as a new array. 
+// The original string is not modified. 
+console.log(gameName.split("-"));//alisha-kh gve o/p alisha ,kh = seperated by '-'
+
+console.log(str.split("country"));
+
+
+
+
 
