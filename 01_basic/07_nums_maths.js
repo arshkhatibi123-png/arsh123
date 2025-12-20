27e6e434a4665f0a3f971ddf495ca27bfedaba15
arshkhatibi123-png <arshkhatibi123@gmail.com>
2025-12-20 08:33:49 +0000

    nums

---
+++ 01_basic/07_nums_maths.js
@@ -0,0 +1,17 @@
+const score = 400
+console.log(score);
+
+
+const balance = new Number(100)// specify it is number
+console.log(balance);
+
+
+console.log(balance.toString().length); // converted to string , provide sm extra prprty
+console.log(balance.toFixed(2)); //100.00=fixed upto 2 decimal value
+
+
+const otherNumber = 23.68934
+console.log(otherNumber.toPrecision(3)); // o/p=23.7
+ 
+
+    
