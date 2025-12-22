fc9746bfd9bbdecd340f1f4eeae81c78db9cee16
arshkhatibi123-png <arshkhatibi123@gmail.com>
2025-12-22 14:22:15 +0000

    dateTime

---
+++ 01_basic/08_dateTime.js
@@ -0,0 +1,10 @@
+// Dates = typeof = object
+
+let myDate = new Date()
+console.log(myDate.toString());// date, time and gmt
+console.log(myDate.toDateString());// only date
+console.log(myDate.toLocaleString());// date and time but date mm/dd/yy
+console.log(typeof myDate);
+
+
+
