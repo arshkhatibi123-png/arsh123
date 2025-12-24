86e6c5f469538a661ac682c275c3b3488cfc58c8
arshkhatibi123-png <arshkhatibi123@gmail.com>
2025-12-24 07:38:29 +0000

    array2

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -1,5 +1,6 @@
 const marvel_heros = ["thor","ironman","spiderman"]
 const dc_heroes = ["superman","flash","batman"]
 
-marvel_heros.push(dc_heroes)// gave 
+marvel_heros.push(dc_heroes)// gave  o/p = array over array, so not use.
 console.log(marvel_heros);
+console.log(marvel_heros)[3][1];
