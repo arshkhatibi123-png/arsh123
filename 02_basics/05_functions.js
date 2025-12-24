53a7ebbc9fb066dca0c70f45c4920c7f4bb6a763
arshkhatibi123-png <arshkhatibi123@gmail.com>
2025-12-24 15:25:34 +0000

    functions

---
+++ 02_basics/05_functions.js
@@ -0,0 +1,18 @@
+//**************FUNCTIONS***************** */
+//Functions in JavaScript are reusable blocks of code 
+// designed to perform specific tasks. 
+// They allow you to organize, reuse, and modularize code.
+//  It can take inputs, perform actions, and return outputs.
+function SUM(X,Y)//function keyword , function name and parameters
+{
+    return X+Y;//return keyword and funtn body
+}
+let result = SUM(4,5);// funtn call and arguments
+console.log(result);
+
+function greet(name)// name= paramtr
+{
+    console.log("Hello " +  name);
+    
+}
+greet("Alisha");// Alisha =arguments
