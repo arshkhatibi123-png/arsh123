8c90a9572d1474b7783e08f2f218b093c60e919c
arshkhatibi123-png <arshkhatibi123@gmail.com>
2025-12-24 10:14:10 +0000

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,12 @@
+//In JavaScript, both arrays and objects are used to store collections of data,
+// but they differ primarily in how data is organized, indexed, and optimized. 
+// Arrays are ideal for ordered, list-like data with numeric indexing,
+//  while objects are better suited for storing related data using named key-value pairs. type or structure	Used for grouping related data using labels (properties)
+
+///*************OBJECT************ */
+
+//Note:
+
+//You should declare objects with the "const keyword".
+// When an object is declared with const, you cannot later reassign it to point to a different variable.
+// It does not make the object unchangeable. You can still modify its properties and values.
