8457c0b0cd48777992a163fe0f77e743c4384521
arshkhatibi123-png <arshkhatibi123@gmail.com>
2025-12-24 08:01:41 +0000

    array2

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -1,6 +1,5 @@
 const marvel_heros = ["thor","ironman","spiderman"]
-const dc_heroes = ["superman","flash","batman"]
-
-marvel_heros.push(dc_heroes)// gave  o/p = array over array, so not use.
+const dc_heroes = ["superman","flash","batman"]//o/p=['Thor','ironman,'spiderman'['superman','flash','batman']]
+marvel_heros.push(dc_heroes) // gave  o/p = array over array, so not use.
 console.log(marvel_heros);
-console.log(marvel_heros)[3][1];
+console.log(marvel_heros[3][1]);// 3rd element 1st
