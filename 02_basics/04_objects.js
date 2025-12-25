ff89c2f4a7b27a5e60d7be87373a8ab1a0faf9ba
arshkhatibi123-png <arshkhatibi123@gmail.com>
2025-12-25 11:19:35 +0000

    objects2

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,1 +1,11 @@
 // objects2
+// singleton
+//const appUser = new Object()// singleton object
+
+const appUser = {}// op same {}, just it is not singleton object
+
+appUser.id = "123abc"
+appUser.name = "rocky"
+appUser.isLoggedIn ="false"
+
+console.log(appUser); // op={}
