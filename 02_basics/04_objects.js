a1c21aae87582f9f9f00613310848c969c254c3e
arshkhatibi123-png <arshkhatibi123@gmail.com>
2025-12-25 11:59:22 +0000

    objects2

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -8,4 +8,19 @@ appUser.id = "123abc"
 appUser.name = "rocky"
 appUser.isLoggedIn ="false"
 
-console.log(appUser); // op={}
+//console.log(appUser); // 1)op={}// 2)now when declr id,name ,loggedi so o/p will b dt under{}
+
+
+// ****Objects under Objects**********
+
+const newUser = {
+    email: "ak14@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname : "Alisha",
+            lastname :"Khatibi"
+        }
+
+}
+}
+console.log(newUser.fullname);
