7861547b48cc1b12db7350174b6a52b0e4c1696b
arshkhatibi123-png <arshkhatibi123@gmail.com>
2025-12-25 13:20:04 +0000

    objects2

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -27,3 +27,12 @@ console.log(newUser.fullname);
 
 //**********Object assigned********** */
 
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a" ,4: "b"}
+const obj3 = {5: "a",6: "e" }
+
+//const obj4 = {obj1, obj2, obj3}
+//console.log(obj4);// o/p= {obj1: {'1':'a','2':'b'},obj2:{'3':'a','4':'b'},obj3:{...}:
+
+const obj4 = Object.assign(obj1, obj2)
+console.log(obj4);
