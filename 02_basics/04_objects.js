1d6decc242d9f1f66b45f5476859e63a072da1d3
arshkhatibi123-png <arshkhatibi123@gmail.com>
2025-12-25 13:36:57 +0000

    Pending changes exported from your codespace

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -35,4 +35,10 @@ const obj3 = {5: "a",6: "e" }
 //console.log(obj4);// o/p= {obj1: {'1':'a','2':'b'},obj2:{'3':'a','4':'b'},obj3:{...}:
 
 const obj4 = Object.assign(obj1, obj2)
-console.log(obj4);
+console.log(obj4);// o/p= {'1':'a','2':'b','3':'a','4':'b'}
+
+//The Object.assign() method copies all enumerable own properties from one or more source objects to a target object.
+//  It performs a shallow copy, meaning that nested objects are not cloned,
+//  but their references are copied to the target object. 
+// This method modifies and returns the target object
+// , making it a powerful tool for object manipulation and merging.
