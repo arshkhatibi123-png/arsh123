daf4404986f92c098d0da44e72b530ddfac725e9
arshkhatibi123-png <arshkhatibi123@gmail.com>
2026-01-03 21:19:25 +0530

    Add files via upload

---
+++ 02_basics/06_objects.js
@@ -0,0 +1,56 @@
+//********API******** */
+//An API is simply a "medium to fetch or send data between interfaces".
+//In JavaScript, an API (Application Programming Interface)
+//  allows different software components to communicate,
+//  either by utilizing built-in browser features, 
+// integrating thir/d-party services, 
+// or creating server-side interactions. 
+
+//Common Uses of APIs in JavaScript
+//JavaScript is heavily used for two main categories of APIs: 
+
+//"Browser APIs:" These are built into your web browser
+//  and extend core JavaScript functionality to control the browser 
+// and manipulate the webpage. Examples 
+// include the DOM (Document Object Model) API 
+// for changing" HTML and CSS," 
+// and the "Geolocation API" for accessing a user's location.
+
+//"Third-Party APIs": 
+// These allow your application to interact with external services 
+// over the web to fetch or send data. 
+// Common examples include the Google Maps API, Twitter API, and weather APIs. 
+
+//Key Concepts
+
+//Asynchronous: API calls are asynchronous,
+//  meaning they don't block the rest of your code from running 
+// while waiting for a response. 
+// This is managed using Promises (.then(), .catch()) 
+// or with async/await syntax.
+
+
+//HTTP Methods: Different types of requests are made using standard HTTP methods
+// , such as GET (retrieve data), POST (send data), PUT (update data)
+// , and DELETE (remove data).
+
+
+//Error Handling: It is crucial to implement error handling 
+// to manage potential network issues or incorrect API responses. 
+
+
+//JSON (JavaScript Object Notation): APIs often send data back and forth in JSON
+//  format, which is easily parsable into JavaScript objects
+
+//{
+  //  "name": "alisha",    //JSON //api in objects
+  //"coursename":"js in hindi",
+  //"price": "free"
+//}
+
+
+[                    //api in  Array
+{},                   //object undr array  
+{},
+{}
+]
