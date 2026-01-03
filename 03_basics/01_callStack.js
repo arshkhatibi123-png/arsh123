e48e28de381660fd8f5d56a29c94a48a951f30c1
arshkhatibi123-png <arshkhatibi123@gmail.com>
2026-01-03 16:09:40 +0000

    callstack

---
+++ 03_basics/01_callStack.js
@@ -0,0 +1,4 @@
+// *********** javascript execution context   *****
+// = how to run file by javascript, how to code execute
+// =run in two phase :
+// 1st global exctn contxt-> refer to "this" varible =@ browser=window object
