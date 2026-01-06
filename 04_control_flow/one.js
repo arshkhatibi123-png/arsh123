c8a3cafbe0201dbe71c5d3b916548bcda309fe7c
arshkhatibi123-png <arshkhatibi123@gmail.com>
2026-01-06 15:37:18 +0000

    one

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,51 +1,62 @@
-// if
+// // if
 
-const isUserloggedIn = true
-const temperature = 41
+// const isUserloggedIn = true
+// const temperature = 41
 
-if (temperature === 40)
-{
-console.log("less than 50");
-}
-else {
-    console.log("temperature is greater than 50");
-}
-console.log("Executed");
-
-
-// == chck value , === als chck data type
+// if (temperature === 40)
+// {
+// console.log("less than 50");
+// }
+// else {
+//     console.log("temperature is greater than 50");
+// }
+// console.log("Executed");
 
 
-const score = 200
-if (score > 100){
-    let power = "fly"
-    console.log(`User Power: ${power} `);
-}
-  //console.log(`User Power: ${power} `); // cant use here bcz dclr inside pvt scope , will find error= pwr not defined
-
-  // ******** shorthand notation ***********
-
- // const balance = 1000 
-  //if (balance > 500) {
-   // console.log("expensive");
- // }
-
-        // or 
-
- //if (balance>500) console.log("expensive"); // implicit scope,but only execute n single line or by using "," ; but not use in practical= usimg ","
+// // == chck value , === als chck data type
 
 
+// const score = 200
+// if (score > 100){
+//     let power = "fly"
+//     console.log(`User Power: ${power} `);
+// }
+//   //console.log(`User Power: ${power} `); // cant use here bcz dclr inside pvt scope , will find error= pwr not defined
 
- const balance = 100
- if (balance < 500){
-    console.log(" In budget");
+//   // ******** shorthand notation ***********
+
+//  // const balance = 1000 
+//   //if (balance > 500) {
+//    // console.log("expensive");
+//  // }
+
+//         // or 
+
+//  //if (balance>500) console.log("expensive"); // implicit scope,but only execute n single line or by using "," ; but not use in practical= usimg ","
+
+
+
+//  const balance = 100
+//  if (balance < 500){
+//     console.log(" In budget");
+//  }
+//  else if (balance<700){
+//     console.log(" budget can improve");
+//  }
+//  else if (balance <1200){
+//     console.log("still little chances ");   
+// }
+// else{
+//     console.log("cant afford");
+// }
+
+const userLoggedIn = true
+const debitcard = true
+
+if (userLoggedIn && debitcard){      //&& = rqr both cndtn to fulfill
+    console.log("allow user to purchase");
  }
- else if (balance<700){
-    console.log(" budget can improve");
+ else {
+    console.log(" sorry not allow");
  }
- else if (balance <1200){
-    console.log("still little chances ");   
-}
-else{
-    console.log("cant afford");
-}
+ 
