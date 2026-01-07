7bc303841bdfdd5951651905198ccbdc87e2355d
arshkhatibi123-png <arshkhatibi123@gmail.com>
2026-01-07 10:11:53 +0000

    switch

---
+++ 04_control_flow/2_switch.js
@@ -0,0 +1,23 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+
+
+
+const month = 3
+
+switch(month) {
+    case value:
+
+    break;
+
+    default:
+        break;
+}
