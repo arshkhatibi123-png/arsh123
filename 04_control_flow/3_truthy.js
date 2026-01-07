9d2533195a2d3a196cb91d99c5efb30c55bc103f
arshkhatibi123-png <arshkhatibi123@gmail.com>
2026-01-07 11:47:30 +0000

    truthy

--- 04_control_flow/3_truthy.js
+++ 04_control_flow/3_truthy.js
@@ -1,5 +1,15 @@
  //    ############# truthy and falsy values   ###############
 
  // falsy values :
-
  // false , 0, -0,  BigInt On , "", null , undefined , NaN
+
+ // Truthy values 
+ //"0",'false'," ",[],{},function(){}
+
+ const userEmail = []
+
+ if (userEmail){
+    console.log("Got user email");
+ } else {
+    console.log("Don't have user email");
+ }
