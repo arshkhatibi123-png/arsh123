ac9260299efcd4476a8ac1cc053cd1b1d5a77aec
arshkhatibi123-png <arshkhatibi123@gmail.com>
2026-01-07 14:37:51 +0000

    one

--- 05_Iterations/1_one.js
+++ 05_Iterations/1_one.js
@@ -1,6 +1,11 @@
 // ******** For *************
 
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
+//for (let index = 0; index < array.length; index++) {
+    //const element = array[index];  
+//}
+
+for(let index = 0 ; index<10 ; index++){
+    const element = index;
+    console.log(element);
     
 }
