dbe0fd4aad2fa4fa6f7c7baaf9a8e0b698d4c454
arshkhatibi123-png <arshkhatibi123@gmail.com>
2026-01-07 15:14:25 +0000

    one

--- 05_Iterations/1_one.js
+++ 05_Iterations/1_one.js
@@ -9,21 +9,18 @@
 //     console.log(element);
 // }
 
-for(let i = 0 ; i <= 10 ; i++){
-    const element = i;
-    if (element == 5){
-        console.log("5 is best number");
-    }
-    console.log(element);
-}
+// for(let i = 0 ; i <= 10 ; i++){
+//     const element = i;
+//     if (element == 5){
+//        // console.log("5 is best number");
+//     }
+//    // console.log(element);
+// }
 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i <= 10; i++) {
     console.log(`Outer loop value: ${i}`);
-    
-    for (let j = 0; j < 10; j++) {
-        const element = array[j];
-        
+    for (let j = 0; j <= 10; j++) {
+        console.log(`Inner loop value ${j} and ineer loop ${i}`); 
     }
-    const element = array[index];
-    
+   
 }
