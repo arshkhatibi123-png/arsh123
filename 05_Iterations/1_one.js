274e1588e9f7cd46c8631e26a2c5a9c539c83b4c
arshkhatibi123-png <arshkhatibi123@gmail.com>
2026-01-07 14:58:23 +0000

    one

--- 05_Iterations/1_one.js
+++ 05_Iterations/1_one.js
@@ -4,7 +4,20 @@
     //const element = array[index];  
 //}
 
-for(let index = 0 ; index<10 ; index++){
+for(let index = 0 ; index <= 10 ; index++){
     const element = index;
     console.log(element);
 }
+
+for(let i = 0 ; i <= 10 ; i++){
+    const element = i;
+    if (element == 5){
+        console.log("5 is best number");
+    }
+    console.log(element);
+}
+
+// for(let index = 0 ; index <= 10 ; index++){
+//     const element = index;
+//     console.log(element);
+// }
