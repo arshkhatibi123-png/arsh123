96a962ef160e6e8e28e29bb06b74174cddf4c917
arshkhatibi123-png <arshkhatibi123@gmail.com>
2026-01-08 12:14:52 +0000

    one

--- 05_Iterations/1_one.js
+++ 05_Iterations/1_one.js
@@ -17,7 +17,7 @@
 //    // console.log(element);
 // }
 
-for (let i = 0; i <= 10; i++) {
+for (let i = 2; i <= 10; i++) {
     console.log(`Outer loop value: ${i}`);
     for (let j = 0; j <= 10; j++) {
        // console.log(`Inner loop value ${j} and inner loop ${i}`); 
