0bf757e5c59837200bdf1cc7ac8e19dd08393665
arshkhatibi123-png <arshkhatibi123@gmail.com>
2026-01-08 12:25:35 +0000

    one

--- 05_Iterations/1_one.js
+++ 05_Iterations/1_one.js
@@ -17,10 +17,18 @@
 //    // console.log(element);
 // }
 
-for (let i = 2; i <= 10; i++) {
-    console.log(`Outer loop value: ${i}`);
-    for (let j = 0; j <= 10; j++) {
-       // console.log(`Inner loop value ${j} and inner loop ${i}`); 
-        console.log(i + '*'+ j +' = '+ i*j );
-    } 
+
+// for (let i = 2; i <= 10; i++) {
+//     console.log(`Outer loop value: ${i}`);
+//     for (let j = 1; j <= 10; j++) {
+//        // console.log(`Inner loop value ${j} and inner loop ${i}`); 
+//         console.log(i + '*'+ j +' = '+ i*j );
+//     } 
+// }
+
+
+let myArray = ["flash", "batman", "superman"]
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);  // op= flash  batman  superman
 }
