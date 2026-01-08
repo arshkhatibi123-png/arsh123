71dc329deb70901203bb98de43a6562b7f884eb7
arshkhatibi123-png <arshkhatibi123@gmail.com>
2026-01-08 12:47:15 +0000

    one

--- 05_Iterations/1_one.js
+++ 05_Iterations/1_one.js
@@ -28,7 +28,7 @@
 
 
 let myArray = ["flash", "batman", "superman"]
-for (let index = 0; index < myArray.length; index++) {
+for (let index = 0; index < myArray.length; index++) {  //if <= o/p give = flash  batman  superman  undefined
     const element = myArray[index];
     console.log(element);  // op= flash  batman  superman
 }
