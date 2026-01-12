03172abc3ac70d1fd3a07a0e8217b1d2a5130be5
arshkhatibi123-png <arshkhatibi123@gmail.com>
2026-01-12 08:08:45 +0000

    while

--- 05_Iterations/2_whilelLoop.js
+++ 05_Iterations/2_whilelLoop.js
@@ -1,11 +1,11 @@
 // ####################   WHILE LOOP    ####################
 
 let index = 0
-while (index <= 10) {
+while (index <= 10)
+ {
 console.log(`Value of index is ${index}`);
 index = index + 2
 }
-
 
 let myArray = ["flash","batman","superman"]
 
