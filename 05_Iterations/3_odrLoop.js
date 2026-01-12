969fe8e2c81f0aa9ece138200a51e23b765bd8b1
arshkhatibi123-png <arshkhatibi123@gmail.com>
2026-01-12 11:48:05 +0000

    loop

--- 05_Iterations/3_odrLoop.js
+++ 05_Iterations/3_odrLoop.js
@@ -3,15 +3,15 @@
 // ["","", ""]
 // [{},{},{}]
 
-const arr = [1,2,3,4,5]
+// const arr = [1,2,3,4,5]
 
-for (const num of arr)
-    {
-    console.log(num); 
-}
+// for (const num of arr)
+//     {
+//     console.log(num); 
+// }
 
 
-const greetings ="Hello world"
+const greetings ="Hello world!"
 for (const greet of greetings) {
-    console.log(`Each char is ${greet}`);
+    console.log(`Each char is ${greet}`)
 }
