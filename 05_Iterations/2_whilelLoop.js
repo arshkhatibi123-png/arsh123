c586ecbc729fa5507a88735ce49ecc92abfe97d7
arshkhatibi123-png <arshkhatibi123@gmail.com>
2026-01-12 10:27:06 +0000

    while

--- 05_Iterations/2_whilelLoop.js
+++ 05_Iterations/2_whilelLoop.js
@@ -1,27 +1,37 @@
 // ####################   WHILE LOOP    ####################
 
-let index = 0
-while (index <= 10)
-{
-console.log(`Value of index is ${index}`);
-index = index + 2
-}
+// let index = 0
+// while (index <= 10)
+// {
+// console.log(`Value of index is ${index}`);
+// index = index + 2
+// }
 
-let myArray = ["flash","batman","superman"]
+// let myArray = ["flash","batman","superman"]
 
-let arr = 0
-while (arr < myArray.length){
+// let arr = 0
+// while (arr < myArray.length){
 
-    console.log(`Value is ${myArray[arr]}`);
-    arr = arr + 1
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr = arr + 1
 
-}
+// }
 
 
 // do while loop
+ 
+// let score = 1
+// do{
+// console.log(`Score is ${score}`);
+// score++
+// }
+// while (score<=10)
 
-do{
-console.log(`Score is $(score)`);
-score++
-}
-while (score<=10)
+
+
+// let score = 11
+// do{
+// console.log(`Score is ${score}`);//op= score is 11
+// score++
+// }
+// while (score<=10)
