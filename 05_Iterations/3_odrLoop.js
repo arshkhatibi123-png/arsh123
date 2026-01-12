c84445988c14d91f1c5fbcf594d593fec58d3a99
arshkhatibi123-png <arshkhatibi123@gmail.com>
2026-01-12 15:34:18 +0000

    loop

--- 05_Iterations/3_odrLoop.js
+++ 05_Iterations/3_odrLoop.js
@@ -11,20 +11,20 @@
 // }
 
 
-const greetings ="Hello world!"
-for (const greet of greetings) {
-    console.log(`Each char is ${greet}`)
-}
+// const greetings ="Hello world!"
+// for (const greet of greetings) {
+//     console.log(`Each char is ${greet}`)
+// }
 
 // Maps
 
 const map = new Map()
     map.set('IN', "India")
-    map.set('USA , "United States of America")
+    map.set('USA' , "United States of America")
     map.set('Fr', "France")
     map.set('IN',"India")
 
 
     console.log(map);
-    
+
     
