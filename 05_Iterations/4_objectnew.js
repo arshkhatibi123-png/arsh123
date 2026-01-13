a85386a6ac2411be4317f1a933d4aaf76bffac54
arshkhatibi123-png <arshkhatibi123@gmail.com>
2026-01-13 15:03:48 +0000

    object

--- 05_Iterations/4_objectnew.js
+++ 05_Iterations/4_objectnew.js
@@ -1,11 +1,17 @@
-const myObject = {
-    js: 'javascript',
-    cpp: 'c++',
-    rb: "ruby",
-    swift: 'swift by apple'
-}
+// const myObject = {
+//     js: 'javascript',
+//     cpp: 'c++',
+//     rb: "ruby",
+//     swift: 'swift by apple'
+// }
+// for (const key in  myObject) { // for in used in object
+//     console.log(`${key} shortcut is for ${myObject[key] }`);
+// }
 
-for (const key of myObject) {
-    console.log(`${key} shortcut is for ${myObject[key] }`);
+// +++for in in array?+++
 
+const programming = ["js", "rb", "py", "java", "cpp"]
+for (const key in programming) {
+   console.log(` ${programming[key] }`);
+   
 }
